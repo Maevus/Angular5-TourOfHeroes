e267e14bbf11e794e2e207a2a723ebb041d9e177
Maeve <maevus@gmail.com>
2018-01-19 12:54:36 +0000

    commit working mock heroes version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,33 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Ngmodel 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+
+import { HttpClientModule } from '@angular/common/http';
+//import { HttpClientInMemoryWebApiModule, InMemoryWebApiModule } from 'angular-in-memory-web-api';
+//import { InMemoryDataService }  from './in-memory-data.service';
+
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HeroService, MessageService],  //MessageService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
